ebb1f04adb43943815d84160e01dca6deebfac51
Jeff Kim <jeff.kim713@gmail.com>
2023-07-27 00:19:02 -0700

    Add custom validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
     self.username = username.downcase
   end
 
+  validate :username_is_not_reserved
   validates :username, uniqueness: true
   validates :username, length: { minimum: 5, message: 'should be at least 5 characters' }
   validates :username, format: { with: /\A[a-zA-Z0-9]+\z/, message: "should be one word" }
+
+  private
+
+  def username_is_not_reserved
+    reserved_usernames = ["jaamongsoda", "superuser"]
+
+    return unless reserved_usernames.include?(username)
+
+    errors.add(:username, "is reserved")
+  end
 end
