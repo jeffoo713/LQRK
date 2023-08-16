b1838c791f88225fdeb7ac8f097e1e42aebc404d
Jeff Kim <jeff.kim713@gmail.com>
2023-08-16 01:24:54 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,17 @@ class User < ApplicationRecord
   end
 
   validate :username_is_not_reserved
-  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :username, length: { minimum: 5, message: 'should be at least 5 characters' }
-  validates :username, format: { with: /\A[a-zA-Z0-9]+\z/, message: "should be one word" }
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'should be one word' }
 
   private
 
   def username_is_not_reserved
-    reserved_usernames = ["jaamongsoda", "superuser"]
+    reserved_usernames = %w[jaamongsoda superuser]
 
     return unless reserved_usernames.include?(username)
 
-    errors.add(:username, "is reserved")
+    errors.add(:username, 'is reserved')
   end
 end
