8f08d26e0201d37961b8bc863312db2421c583f0
Jeff Kim <jeff.kim713@gmail.com>
2023-09-10 17:04:29 -0700

    create signin form and style it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import './App.scss';
+
 const App: React.FC = () => {
+  const handleSignIn = () => {
+    console.log('trying to sign in');
+  };
   return (
-    <div>
-      <h1>Welcome to LQRK</h1>
+    <div className='app'>
+      <h1 className='app-title'>Welcome to LQRK</h1>
+      <form className='app-signin-form'>
+        <input type='text' placeholder='Start with your username!' />
+        <button type='button' onClick={handleSignIn}>
+          SIGN IN
+        </button>
+      </form>
     </div>
   );
 };
