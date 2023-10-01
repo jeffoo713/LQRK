f6cefbada8c991dceb69720085269cf5a8c0c7bd
Jeff Kim <jeff.kim713@gmail.com>
2023-10-01 00:27:30 -0700

    implement root with react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import GlobalProvider from './stateManagement/GlobalProvider';
 
 import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const container = document.getElementById('lqrk-app')!;
 const root = createRoot(container);
 
 root.render(
   <GlobalProvider>
-    <App />
+    <RouterProvider router={router} />
   </GlobalProvider>
 );
